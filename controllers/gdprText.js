a2ae9615a455c5f54247c3dd0b4c6731dfaeb386
Tomáš Javorský <tomasjavorsky@users.noreply.github.com>
2019-11-18 21:20:54 +0100

    changed gdpr text location

--- controllers/gdprText.js
+++ controllers/gdprText.js
@@ -1,21 +1,9 @@
 const getGdprText = (req, res, db) => {
-  return db('gdpr').select('*').orderBy('gdpr_id', "desc").limit(1)
+  db('download_files').select('*').where({file_name: 'Obchodné podmienky'})
     .then(data => res.json(data))
     .catch(err => res.status(400).json('unable to get gdpr text from database\n' + err));
 };
 
-const addGdprText = (req, res, db) => {
-  const {gdprText} = req.body;
-  console.log("adding gdpr text");
-  db('gdpr')
-    .insert({
-      gdpr_text: gdprText
-    })
-    .then(res.json("gdpr_text inserted"))
-    .catch(err => res.status(400).json('Unable to add gdpr text\n' + err));
-};
-
 module.exports={
   getGdprText: getGdprText,
-  addGdprText: addGdprText,
 };
